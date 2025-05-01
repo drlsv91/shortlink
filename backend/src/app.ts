a5072d9e50591dc24357630be9fe9acfe9506fb0
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-05-01 12:11:48 +0100

    Add helmet for request headers security

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express, { Application } from "express";
 import { configService } from "./config";
 import urlRoutes from "./routes/urlRoutes";
+import helmet from "helmet";
 
 const app: Application = express();
 
@@ -10,6 +11,12 @@ app.use(
   cors({
     origin: configService.get("CORS_ORIGIN", "http://localhost:5173").split(","),
     methods: ["GET", "POST"],
+  })
+);
+app.use(
+  helmet({
+    xssFilter: true,
+    noSniff: true,
   })
 );
 
